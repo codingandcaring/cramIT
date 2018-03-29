11c5bbda00819a4ea0d18e1bc5de4738e179c030
Ashley Parker <aparker7698@att.net>
2018-03-29 15:14:50 -0400

    Refactored code for database query

--- database.js
+++ database.js
@@ -6,14 +6,7 @@ const db = pg(dbconfig);
 
 
 let findUser = (attribute, input) => {
-    return new Promise ( (resolve, reject) => {
-        db.query(`SELECT * FROM users WHERE ${attribute} = '${input}';`)
-        .then( (user) => {
-            (pg.end);
-            resolve(user);
-            reject('No user found');
-        })
-    })
+    return db.query(`SELECT * FROM users WHERE ${attribute} = '${input}';`)
 }
 
 module.exports = findUser;
