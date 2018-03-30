e7058a22807ea68aca9ffa0d18ab3b13d9513e8b
Prathyusha Mattam <prathyusha.mattam@gmail.com>
2018-03-30 15:41:07 -0400

    Functionality for the new user after successful creation of account redirecting to the login page and now be able to login

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -29,7 +29,7 @@ let mapFromLoginForm = (username, password) => {
 let userLogin = (event) => {
     // Have to prevent the event default else it will default the request to 'GET' method
     event.preventDefault();
-    let form = document.querySelector('body > div > div.pop-up-login.active > form')
+    let form = document.querySelector('body > div > div.pop-up-login.active > form');
     let userData = mapFromLoginForm(form.username.value, form.password.value);
     form.reset();
     fetch('/tokens', {
@@ -53,11 +53,37 @@ let userLogin = (event) => {
     })
 };
 
+let mapFromCreateAccountForm = (username, password, email, location) => {
+    let userData = {
+        "username": username,
+        "password": password,
+        "email": email,
+        "location": location
+    }
+    return userData;
+};
+
+// After successfully creating account for new user redirecting 
+// the user to login page to login
 let createNewAccount = (event) => {
     event.preventDefault();
-    let form = document.querySelector('body > div > div.pop-up-create-account.active > form')
+    let form = document.querySelector('body > div > div.pop-up-create-account.active > form');
+    let userData = mapFromCreateAccountForm(form.username.value, form.password.value, form.email.value, form.location.value);
+    console.log(userData);
     form.reset();
-    console.log(event);
+    fetch('/signup', {
+        method: 'POST',
+        body: JSON.stringify(userData),
+        headers: new Headers({
+            'Content-Type': 'application/json'
+        })
+    }).then(response => {
+        if (response.status === 200) {
+            document.location.href = '/index.html';
+        } else {
+            console.log("Can't create account");
+        }
+    })
 };
 
 let closeWindow = (event) => {
